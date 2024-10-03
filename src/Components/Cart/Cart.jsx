9834947cdc175f9ac4aa6e50f901103ba2b1a4a8
sree00098 <168292864+sree00098@users.noreply.github.com>
2024-10-04 01:38:20 +0530

    added functionality cart

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Cart = () => {
+  const [items , setItems] = useState();
+  useEffect(()=>{
+    const fetchCartData = async() => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    if(user){
+    try{
+        const cartCollection = collection(db,'Cart',user.uid,'CartItems')
+        const querySnapshot = await getDocs(cartCollection)
+        const cartItems = querySnapshot.docs.map(doc=>{
+            const data = doc.data();
+            console.log(data)
+            return{
+                id:doc.id,
+                title:data.pName,
+                price:data.price,
+                image:data.imageUrl
+            }
+        })
+        console.log("Data Fetched")
+        setItems(cartItems)
+    }
+    catch(error){
+        console.error("Error fetching data")
+    }
+    }
+    }
+    fetchCartData();
+},[])
   return (
-    <div>Cart</div>
+    
+    <div>
+
+    
+    </div>
   )
 }
 
